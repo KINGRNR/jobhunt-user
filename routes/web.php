d14f1bd99228ee6b7ace5976027661c02682c783
rawhx_x <achmadhasbil24@gmail.com>
2023-07-12 20:57:56 +0700

    logout e double:v

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout'])->name('login.logout');
     Route::get('/login', [AuthController::class, 'index'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.store');
     Route::get('/register', function () {
