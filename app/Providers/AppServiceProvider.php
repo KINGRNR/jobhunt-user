ef36250063f51fde017e2544a94d31c59a8c66f1
rawhx_x <achmadhasbil24@gmail.com>
2023-07-12 21:18:13 +0700

    konfigurasi waktu time -INA

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        config(['app.locale' => 'id']);
+        config(['app.locale' => 'id']); tes
         Carbon::setLocale('id');
         date_default_timezone_set('asia/jakarta');
     }
