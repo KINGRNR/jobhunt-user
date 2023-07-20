2e070e29b4643ad6833bc0b995b46240bbbf7eb8
davidisnthere <119030792+davidisnthere@users.noreply.github.com>
2023-07-20 07:53:47 +0700

    nggeh
    ea

---
+++ resources/views/category/categories.blade.php
@@ -0,0 +1,17 @@
+<div class="grid grid-cols-2">
+    <div class="flex">
+    <div class="flex-none bg-figma-agakbiru">
+        <div class="block ml-12 mr-40 my-12">
+        <p class="mb-8 text-xl font-semibold text-white">Job Type</p>
+        <div class="flex items-center mb-6">
+            <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-figma-agakbiru bg-white border-white checked:ring-white checked:ring-2 duration-75">
+            <label for="default-radio-1" class="ml-2 font-medium text-white">All</label>
+        </div>
+        <div class="flex items-center">
+            <input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-figma-agakbiru bg-white border-white checked:ring-white checked:ring-2 duration-75">
+            <label for="default-radio-2" class="ml-2 font-medium text-white">Checked state</label>
+        </div>
+        </div>
+    </div>
+</div>
+</div>
