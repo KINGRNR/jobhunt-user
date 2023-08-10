0d1e32b8e16abb52b691c7fde58173acbf32238e
rawhx_x <achmadhasbil24@gmail.com>
2023-08-10 15:14:10 +0700

    wkwk perbaikan name logout:v

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout'])->name('login.logout');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 Route::middleware(['guest'])->group(function () {
