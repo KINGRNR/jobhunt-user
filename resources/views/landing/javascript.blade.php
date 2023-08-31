837f2fcd9e885792c5bc4719ba5e80f37c46a0a3
Rafif <rafifnr06@gmail.com>
2023-08-31 21:23:46 +0700

    update redirect id & routing

---
+++ resources/views/landing/javascript.blade.php
@@ -0,0 +1,69 @@
+<script>
+    var jobs = [];
+    $(() => {
+        init()
+    });
+    init = () => {
+        loadcount();
+    }
+    redirectKategori = (button) => {
+        const id = button.getAttribute('data-id');
+        var params = [];
+        switch (id) {
+            case '1':
+                params = "jbhnt-ti1cci"
+                break;
+            case '2':
+                params = "jbhnt-ekjjh2";
+                break;
+            case '3':
+                params = "jbhnt-TekIjkaa3";
+                break;
+            case '4':
+                params = "jbhnt-SSjsj4";
+                break;
+            case '5':
+                params = "jbhnt-pddknJsst5";
+                break;
+            case '6':
+                params = "jbhnt-perTrav6";
+                break;
+            case '7':
+                params = "jbhnt-FoBevarss7";
+                break;
+            case '8':
+                params = "jbhnt-shwallss8";
+                break;
+            default:
+                break;
+        }
+        location.href = '/category?id=' + params;
+    }
+
+    loadcount = () => {
+        $.ajax({
+            url: '/jobscount',
+            type: 'GET',
+            success: function(response) {
+
+                response.jobCounts.forEach(function(item) {
+                    const inputId = `kategori${item.job_category}`;
+
+                    const inputElement = document.getElementById(inputId);
+                    console.log(inputElement);
+                    if (inputElement) {
+                        if (item.count > 0) {
+                            inputElement.innerHTML = item.count + " Open Position";
+                        } else {
+                            inputElement.innerHTML = "No job available";
+                        }
+                    }
+
+                });
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    }
+</script>
