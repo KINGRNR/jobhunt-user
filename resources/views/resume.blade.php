5e73130f85e0e020cac22bfa119402097bdd73dc
davidisnthere <119030792+davidisnthere@users.noreply.github.com>
2023-09-10 18:50:56 +0700

    ignore the package.json please
    added WYSIWYG editor (albeit temporarily) for resume
    prone to change so don't expect much

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.cdnfonts.com/css/poppins" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link
     rel="stylesheet"
@@ -20,5 +21,4 @@
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.6/flowbite.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
 </html>
