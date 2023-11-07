a8a7de4ab2cc1dc30ac1594a237febec691e682d
Rafif <rafifnr06@gmail.com>
2023-11-07 14:57:40 +0700

    Feat Job Vacancy & Job Detail, Refactor Route

---
+++ resources/views/company/detailjob/hero.blade.php
@@ -0,0 +1,18 @@
+<div class="mt-5 grid">
+  <div class="w-full max-w-full mb-6 mt-20">
+      <div class="bg-black relative flex flex-col min-w-0" style="background-color: #EEEFF0;">
+          <div class="flex-auto p-10">
+              <div class="flex flex-row ">
+                  <div class="container mx-auto">
+                      <div>
+                          <h1 class="mb-2 font-bold dark:text-white">Dashboard</h1>
+                          <p
+                              class="mb-0 font-sans text-sm font-semibold leading-normal dark:text-white dark:opacity-60">
+                              Home - <span class="font-bold">Detail Job Vacancy</span></p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
